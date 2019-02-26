8e34f13d2d8816d932a45f9804e84bfd6195c59c
yr8vy <yr8vy@mathcs01.richmond.edu>
2019-02-25 20:35:16 -0500

    Added files

---
+++ lab6/OpcodeTable.cpp
@@ -0,0 +1,233 @@
+#include "OpcodeTable.h"
+
+OpcodeTable::OpcodeTable()
+// Initializes all the fields for every instruction in Opcode enum
+{
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+ 
+  //Your code here
+  
+  myArray[ADDI].name = "addi"; myArray[ADD].numOps = 2;  
+  myArray[ADD].rdPos = -1; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 0; 
+  myArray[ADD].immPos = 2;
+  myArray[ADD].instType = ITYPE;
+  myArray[ADD].op_field = "001000"; myArray[ADD].name = "sub";
+  myArray[ADD].numOps = 3;  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100010";
+ 
+  myArray[ADD].name = "mult";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = -; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "011000";
+ 
+  myArray[ADD].name = "mfhi";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = -1; 
+  myArray[ADD].rsPos = 0;
+  myArray[ADD].rtPos = 1; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "010000";
+  
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000"; 
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+  myArray[ADD].name = "add";
+  myArray[ADD].numOps = 3;  
+  myArray[ADD].rdPos = 0; 
+  myArray[ADD].rsPos = 1;
+  myArray[ADD].rtPos = 2; 
+  myArray[ADD].immPos = -1;
+  myArray[ADD].instType = RTYPE;
+  myArray[ADD].op_field = "000000";
+  myArray[ADD].funct_field = "100000";
+
+}
+
+
+
+
+Opcode OpcodeTable::getOpcode(string str)
+// Given a valid MIPS assembly mnemonic, returns an Opcode which represents a 
+// template for that instruction.
+{
+  for(int i = 0; i < (int)UNDEFINED; i++){
+    if(myArray[i].name == str){
+      return (Opcode)i;
+    }
+  }
+  return UNDEFINED;
+}
+
+int OpcodeTable::numOperands(Opcode o)
+// Given an Opcode, returns number of expected operands.
+{
+  if(o < 0 || o >= UNDEFINED)
+    return -1;
+
+  return myArray[o].numOps;
+}
+
+
+int OpcodeTable::RSposition(Opcode o)
+// Given an Opcode, returns the position of RS field.  If field is not
+// appropriate for this Opcode, returns -1.
+{
+  if(o < 0 || o >= UNDEFINED)
+    return -1;
+
+  return myArray[o].rsPos;
+}
+
+int OpcodeTable::RTposition(Opcode o)
+// Given an Opcode, returns the position of RT field.  If field is not
+// appropriate for this Opcode, returns -1.
+{
+  if(o < 0 || o >= UNDEFINED)
+    return -1;
+
+  return myArray[o].rtPos;
+}
+
+int OpcodeTable::RDposition(Opcode o)
+// Given an Opcode, returns the position of RD field.  If field is not
+// appropriate for this Opcode, returns -1.
+{
+  if(o < 0 || o >= UNDEFINED)
+    return -1;
+
+  return myArray[o].rdPos;
+}
+
+int OpcodeTable::IMMposition(Opcode o)
+// Given an Opcode, returns the position of IMM field.  If field is not
+// appropriate for this Opcode, returns -1.
+{
+  if(o < 0 || o >= UNDEFINED)
+    return -1;
+
+  return myArray[o].immPos;
+}
+
+InstType OpcodeTable::getInstType(Opcode o)
+// Given an Opcode, returns instruction type.
+{
+  if(o < 0 || o > UNDEFINED)
+    return (InstType)-1;
+  return myArray[o].instType;
+}
+
+string OpcodeTable::getOpcodeField(Opcode o)
+// Given an Opcode, returns a string representing the binary encoding of the opcode
+// field.
+{
+  if(o < 0 || o > UNDEFINED)
+    return string("");
+  return myArray[o].op_field;
+}
+
+string OpcodeTable::getFunctField(Opcode o)
+// Given an Opcode, returns a string representing the binary encoding of the function
+// field.
+{
+  if(o < 0 || o > UNDEFINED)
+    return string("");
+  return myArray[o].funct_field;
+}
+
+bool OpcodeTable::isIMMLabel(Opcode o)
+// Given an Opcode, returns true if instruction expects a label in the instruction.
+// See "J".
+{
+  if(o < 0 || o > UNDEFINED)
+    return false;
+  return myArray[o].immLabel;
+}
